7f620a7aa9e2af4492a0f1d11d470d23f2253a74
Gabriel Ximenes Mitozo Noronha <gabriel.noronha@grupotapajos.com.br>
2024-12-26 08:52:47 -0400

    feat:add new styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,33 +4,38 @@ import {
   StyleSheet,
   TextInput,
   View,
+  Text,
   Button,
   Alert,
+  Pressable,
 } from "react-native";
-import { Camera, CameraView } from "expo-camera"; 
+import { Camera, CameraView } from "expo-camera";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { Picker } from "@react-native-picker/picker";
 import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from '@react-navigation/stack';
+import { StackNavigationProp } from "@react-navigation/stack";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export type RootStackParamList = {
   Home: undefined;
-  explore: { eanList: { competitor: string; ean: string; price: string }[] }; 
+  explore: { eanList: { competitor: string; ean: string; price: string }[] };
 };
 
-type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, "Home">;
 
 export default function HomeScreen() {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [scanning, setScanning] = useState(false);
-  const [ean, setEan] = useState<string>(""); 
-  const [price, setPrice] = useState<string>(""); 
+  const [ean, setEan] = useState<string>("");
+  const [price, setPrice] = useState<string>("");
   const [selectedStore, setSelectedStore] = useState<string>("Drogasil");
   const [scanned, setScanned] = useState(false);
 
-  const [eanList, setEanList] = useState<{ competitor: string; ean: string; price: string }[]>([]);
+  const [eanList, setEanList] = useState<
+    { competitor: string; ean: string; price: string }[]
+  >([]);
 
   const navigation = useNavigation<HomeScreenNavigationProp>();
 
@@ -43,20 +48,18 @@ export default function HomeScreen() {
 
   const onBarcodeRead = ({ type, data }: { type: string; data: string }) => {
     console.log("Código de barras lido:", data);
-    setEan(data); 
-    setScanning(false); 
-    fetchProductByEan(data); // Adiciona a busca do produto
+    setEan(data);
+    setScanning(false);
+    fetchProductByEan(data);
   };
 
-  // Função para buscar os produtos na API com o EAN
-  // Função para buscar os produtos na API com o EAN
   const fetchProductByEan = async (ean: string) => {
     try {
-      const response = await fetch('http://10.2.10.202:5034/api/filtro_por', {
-        method: 'POST',
+      const response = await fetch("http://10.2.10.202:5034/api/filtro_por", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'accept': 'application/json',
+          "Content-Type": "application/json",
+          accept: "application/json",
         },
         body: JSON.stringify({
           filtros: [
@@ -69,37 +72,36 @@ export default function HomeScreen() {
           page_size: 24,
         }),
       });
-  
+
       const data = await response.json();
-      console.log('Produtos encontrados:', data); // Log dos produtos encontrados
+      console.log("Produtos encontrados:", data);
       if (data && data.length > 0) {
-        // Se produtos forem encontrados, adicione-os à lista
         const newEanList = data.map((product: any) => ({
-          competitor: product.tabela, // Usando o campo 'tabela' como loja
-          ean: product.ean.toString(), // Convertendo o EAN para string
+          competitor: product.tabela,
+          ean: product.ean.toString(),
           price: product.preco.toString(), // Convertendo o preço para string
         }));
-  
+
         // Verifica se o EAN já foi adicionado à lista para evitar duplicatas
-        setEanList(prevList => {
+        setEanList((prevList) => {
           const updatedList = [...prevList];
           newEanList.forEach((item: any) => {
-            if (!updatedList.some(existingItem => existingItem.ean === item.ean)) {
+            if (
+              !updatedList.some((existingItem) => existingItem.ean === item.ean)
+            ) {
               updatedList.push(item);
             }
           });
           return updatedList;
         });
       } else {
-        Alert.alert('Nenhum produto encontrado com esse EAN.');
+        Alert.alert("Nenhum produto encontrado com esse EAN.");
       }
     } catch (error) {
-      console.error('Erro ao buscar produtos:', error);
-      Alert.alert('Erro ao buscar produtos. Tente novamente mais tarde.');
+      console.error("Erro ao buscar produtos:", error);
+      Alert.alert("Erro ao buscar produtos. Tente novamente mais tarde.");
     }
   };
-  
-
 
   const handleEanChange = (value: string) => {
     setEan(value);
@@ -122,14 +124,20 @@ export default function HomeScreen() {
     );
   }
 
-  const handleBarCodeScanned = ({ type, data }: { type: string; data: string }) => {
-    console.log("Código de barras lido:", data); 
-    setEan(data); 
-    setScanning(false); 
+  const handleBarCodeScanned = ({
+    type,
+    data,
+  }: {
+    type: string;
+    data: string;
+  }) => {
+    console.log("Código de barras lido:", data);
+    setEan(data);
+    setScanning(false);
 
-    setEanList(prevList => [
+    setEanList((prevList) => [
       ...prevList,
-      { competitor: selectedStore, ean: data, price: price } 
+      { competitor: selectedStore, ean: data, price: price },
     ]);
   };
 
@@ -137,13 +145,13 @@ export default function HomeScreen() {
     if (eanList.length === 0) {
       Alert.alert("A lista de EANs está vazia.");
     } else {
-      navigation.navigate('explore', { eanList }); 
+      navigation.navigate("explore", { eanList });
     }
   };
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerBackgroundColor={{ light: "#007933", dark: "#007933" }}
       headerImage={
         <Image
           source={require("@/assets/images/grupo-tapajos.png")}
@@ -151,12 +159,10 @@ export default function HomeScreen() {
         />
       }
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Insira o preço do produto!</ThemedText>
-      </ThemedView>
-
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Selecione a loja:</ThemedText>
+        <ThemedText type="subtitle" style={styles.subtitle}>
+          Selecione a loja:
+        </ThemedText>
         <Picker
           selectedValue={selectedStore}
           onValueChange={(itemValue) => setSelectedStore(itemValue)}
@@ -170,7 +176,9 @@ export default function HomeScreen() {
       </ThemedView>
 
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Insira o EAN do produto:</ThemedText>
+        <ThemedText type="subtitle" style={styles.subtitle}>
+          Insira o EAN do produto:
+        </ThemedText>
         <TextInput
           style={styles.input}
           value={ean}
@@ -178,7 +186,13 @@ export default function HomeScreen() {
           placeholder="EAN"
           keyboardType="numeric"
         />
-        <Button title="Ler código EAN" onPress={() => setScanning(true)} />
+
+        <Pressable
+          style={styles.cameraButton}
+          onPress={() => setScanning(true)}
+        >
+          <AntDesign name="camera" size={24} color="black" />
+        </Pressable>
       </ThemedView>
 
       {scanning && (
@@ -191,7 +205,7 @@ export default function HomeScreen() {
             }}
           >
             <View style={styles.overlay}>
-              <ThemedText type="subtitle">
+              <ThemedText type="subtitle" style={styles.subtitle}>
                 Posicione o código de barras no campo
               </ThemedText>
             </View>
@@ -200,7 +214,9 @@ export default function HomeScreen() {
       )}
 
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Insira o preço do produto:</ThemedText>
+        <ThemedText type="subtitle" style={styles.subtitle}>
+          Insira o preço do produto:
+        </ThemedText>
         <TextInput
           style={styles.input}
           value={price}
@@ -209,22 +225,14 @@ export default function HomeScreen() {
           keyboardType="decimal-pad"
         />
       </ThemedView>
-
-      <Button title="Enviar EANs" onPress={navigateToExplore} />
+      <Pressable style={styles.button} onPress={navigateToExplore}>
+        <Text style={styles.buttonText}>Confirmar</Text>
+      </Pressable>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
   reactLogo: {
     height: 250,
     width: 490,
@@ -232,14 +240,23 @@ const styles = StyleSheet.create({
     left: 0,
     position: "absolute",
   },
-  input: {
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    paddingLeft: 8,
-    marginTop: 8,
-    borderRadius: 4,
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
+
+  overlay: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -100 }, { translateY: -20 }],
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 10,
+    borderRadius: 5,
+  },
+
   cameraContainer: {
     flex: 1,
     justifyContent: "center",
@@ -251,19 +268,51 @@ const styles = StyleSheet.create({
     height: 300,
     borderRadius: 10,
   },
-  overlay: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: [{ translateX: -100 }, { translateY: -20 }],
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  stepContainer: {
+    padding: 20,
+  },
+  picker: {
+    marginVertical: 10,
+    height: 50,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
     padding: 10,
-    borderRadius: 5,
+    marginBottom: 15,
+    backgroundColor: "#f9f9f9",
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#007933",
+    paddingVertical: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginHorizontal: 20,
   },
 
-  picker: {
-    height: 50,
-    width: "100%",
-    backgroundColor: "#fff",
+  cameraButton: {
+    backgroundColor: "#007933",
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 40,
+    right: 25,
+  },
+  cameraIcon: {
+    width: 30,
+    height: 30,
   },
 });
